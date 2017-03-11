5b5e5c7084b035cd7635e8d8a67cd4ab38e77ae4
KermiteO <kermicheo@gmail.com>
2017-03-11 13:30:50 -0800

    Add files via upload

---
+++ debug/moc_mainwindow.cpp
@@ -0,0 +1,252 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QQueue>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[37];
+    char stringdata0[693];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 21), // "on_normalTrip_clicked"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 22), // "on_CustomTrip1_clicked"
+QT_MOC_LITERAL(4, 57, 22), // "on_CustomTrip2_clicked"
+QT_MOC_LITERAL(5, 80, 20), // "on_startTrip_clicked"
+QT_MOC_LITERAL(6, 101, 13), // "FillTripTable"
+QT_MOC_LITERAL(7, 115, 12), // "FillItemMenu"
+QT_MOC_LITERAL(8, 128, 8), // "CityName"
+QT_MOC_LITERAL(9, 137, 18), // "FillAdminTableView"
+QT_MOC_LITERAL(10, 156, 11), // "FillAdminCB"
+QT_MOC_LITERAL(11, 168, 22), // "on_LogInButton_clicked"
+QT_MOC_LITERAL(12, 191, 21), // "on_pass_returnPressed"
+QT_MOC_LITERAL(13, 213, 24), // "on_LogOut_Button_clicked"
+QT_MOC_LITERAL(14, 238, 26), // "on_LogOut_Button_2_clicked"
+QT_MOC_LITERAL(15, 265, 14), // "clearCityTable"
+QT_MOC_LITERAL(16, 280, 15), // "clearItemsTable"
+QT_MOC_LITERAL(17, 296, 24), // "clearselectedCitiesTable"
+QT_MOC_LITERAL(18, 321, 15), // "ClearAdminTable"
+QT_MOC_LITERAL(19, 337, 24), // "on_addCityButton_clicked"
+QT_MOC_LITERAL(20, 362, 39), // "on_RemoveItemCityCB_currentIn..."
+QT_MOC_LITERAL(21, 402, 4), // "arg1"
+QT_MOC_LITERAL(22, 407, 31), // "on_AI_CN_CB_currentIndexChanged"
+QT_MOC_LITERAL(23, 439, 24), // "on_AddItemButton_clicked"
+QT_MOC_LITERAL(24, 464, 27), // "on_RemoveItemButton_clicked"
+QT_MOC_LITERAL(25, 492, 41), // "on_LocationsTableWidget_cellD..."
+QT_MOC_LITERAL(26, 534, 3), // "row"
+QT_MOC_LITERAL(27, 538, 6), // "column"
+QT_MOC_LITERAL(28, 545, 30), // "on_NextCity_pushButton_clicked"
+QT_MOC_LITERAL(29, 576, 30), // "on_Purchase_pushButton_clicked"
+QT_MOC_LITERAL(30, 607, 8), // "SortTrip"
+QT_MOC_LITERAL(31, 616, 15), // "QQueue<QString>"
+QT_MOC_LITERAL(32, 632, 4), // "temp"
+QT_MOC_LITERAL(33, 637, 8), // "currCity"
+QT_MOC_LITERAL(34, 646, 4), // "Find"
+QT_MOC_LITERAL(35, 651, 8), // "srchCity"
+QT_MOC_LITERAL(36, 660, 32) // "on_NextCity_pushButton_2_clicked"
+
+    },
+    "MainWindow\0on_normalTrip_clicked\0\0"
+    "on_CustomTrip1_clicked\0on_CustomTrip2_clicked\0"
+    "on_startTrip_clicked\0FillTripTable\0"
+    "FillItemMenu\0CityName\0FillAdminTableView\0"
+    "FillAdminCB\0on_LogInButton_clicked\0"
+    "on_pass_returnPressed\0on_LogOut_Button_clicked\0"
+    "on_LogOut_Button_2_clicked\0clearCityTable\0"
+    "clearItemsTable\0clearselectedCitiesTable\0"
+    "ClearAdminTable\0on_addCityButton_clicked\0"
+    "on_RemoveItemCityCB_currentIndexChanged\0"
+    "arg1\0on_AI_CN_CB_currentIndexChanged\0"
+    "on_AddItemButton_clicked\0"
+    "on_RemoveItemButton_clicked\0"
+    "on_LocationsTableWidget_cellDoubleClicked\0"
+    "row\0column\0on_NextCity_pushButton_clicked\0"
+    "on_Purchase_pushButton_clicked\0SortTrip\0"
+    "QQueue<QString>\0temp\0currCity\0Find\0"
+    "srchCity\0on_NextCity_pushButton_2_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      27,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  149,    2, 0x08 /* Private */,
+       3,    0,  150,    2, 0x08 /* Private */,
+       4,    0,  151,    2, 0x08 /* Private */,
+       5,    0,  152,    2, 0x08 /* Private */,
+       6,    0,  153,    2, 0x08 /* Private */,
+       7,    1,  154,    2, 0x08 /* Private */,
+       9,    1,  157,    2, 0x08 /* Private */,
+      10,    0,  160,    2, 0x08 /* Private */,
+      11,    0,  161,    2, 0x08 /* Private */,
+      12,    0,  162,    2, 0x08 /* Private */,
+      13,    0,  163,    2, 0x08 /* Private */,
+      14,    0,  164,    2, 0x08 /* Private */,
+      15,    0,  165,    2, 0x08 /* Private */,
+      16,    0,  166,    2, 0x08 /* Private */,
+      17,    0,  167,    2, 0x08 /* Private */,
+      18,    0,  168,    2, 0x08 /* Private */,
+      19,    0,  169,    2, 0x08 /* Private */,
+      20,    1,  170,    2, 0x08 /* Private */,
+      22,    1,  173,    2, 0x08 /* Private */,
+      23,    0,  176,    2, 0x08 /* Private */,
+      24,    0,  177,    2, 0x08 /* Private */,
+      25,    2,  178,    2, 0x08 /* Private */,
+      28,    0,  183,    2, 0x08 /* Private */,
+      29,    0,  184,    2, 0x08 /* Private */,
+      30,    2,  185,    2, 0x08 /* Private */,
+      34,    1,  190,    2, 0x08 /* Private */,
+      36,    0,  193,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void, QMetaType::QString,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   21,
+    QMetaType::Void, QMetaType::QString,   21,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   26,   27,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 31, QMetaType::QString,   32,   33,
+    QMetaType::Bool, QMetaType::QString,   35,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_normalTrip_clicked(); break;
+        case 1: _t->on_CustomTrip1_clicked(); break;
+        case 2: _t->on_CustomTrip2_clicked(); break;
+        case 3: _t->on_startTrip_clicked(); break;
+        case 4: _t->FillTripTable(); break;
+        case 5: _t->FillItemMenu((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 6: _t->FillAdminTableView((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 7: _t->FillAdminCB(); break;
+        case 8: _t->on_LogInButton_clicked(); break;
+        case 9: _t->on_pass_returnPressed(); break;
+        case 10: _t->on_LogOut_Button_clicked(); break;
+        case 11: _t->on_LogOut_Button_2_clicked(); break;
+        case 12: _t->clearCityTable(); break;
+        case 13: _t->clearItemsTable(); break;
+        case 14: _t->clearselectedCitiesTable(); break;
+        case 15: _t->ClearAdminTable(); break;
+        case 16: _t->on_addCityButton_clicked(); break;
+        case 17: _t->on_RemoveItemCityCB_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 18: _t->on_AI_CN_CB_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 19: _t->on_AddItemButton_clicked(); break;
+        case 20: _t->on_RemoveItemButton_clicked(); break;
+        case 21: _t->on_LocationsTableWidget_cellDoubleClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 22: _t->on_NextCity_pushButton_clicked(); break;
+        case 23: _t->on_Purchase_pushButton_clicked(); break;
+        case 24: _t->SortTrip((*reinterpret_cast< QQueue<QString>(*)>(_a[1])),(*reinterpret_cast< QString(*)>(_a[2]))); break;
+        case 25: { bool _r = _t->Find((*reinterpret_cast< QString(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = _r; }  break;
+        case 26: _t->on_NextCity_pushButton_2_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 24:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QQueue<QString> >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 27)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 27;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 27)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 27;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
